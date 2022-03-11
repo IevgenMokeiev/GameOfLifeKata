e782e51466d436a326b2cbc549abc64b87002e3c
Eugene Mokeiev <ievgen.mokeiev@takeaway.com>
2022-03-11 16:13:11 +0200

    initial

---
+++ GameOfLifeKata/GameOfLifeKata/GameOfLifeKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeKataApp.swift
+//  GameOfLifeKata
+//
+//  Created by Yevhen Mokeiev on 11.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
